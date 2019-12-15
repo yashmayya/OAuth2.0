ccfda6c3bed5fef5dddd2910b183df06793e09ed
yashmayya <yash.mayya@gmail.com>
2019-12-15 18:43:04 +0530

    Added basic login page

--- project.py
+++ project.py
@@ -19,7 +19,7 @@ session = DBSession()
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
     login_session['state'] = state
-    return "The current session state is %s" % login_session['state']
+    return render_template("login.html")
 
 
 
