2c9b51c30a96aab65f54efeb40cffcaf8b9eb642
yashmayya <yash.mayya@gmail.com>
2019-12-15 18:36:07 +0530

    Added generation of anti-forgery state token

--- project.py
+++ project.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
+from flask import session as login_session
+import random, string
 
 #Connect to Database and create database session
 engine = create_engine('sqlite:///restaurantmenu.db')
@@ -12,6 +14,13 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    login_session['state'] = state
+    return "The current session state is %s" % login_session['state']
+
 
 
 #JSON APIs to view Restaurant Information
